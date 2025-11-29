84ff7003acd6a47e573a1a6a0cf8726274c38488
bfalls <bfalls@outlook.com>
2025-11-29 13:33:45 -0700

    Add Android theme colors

---
+++ android/app/src/main/java/com/bfalls/suntimealerts/ui/theme/Spacing.kt
@@ -0,0 +1,15 @@
+package com.bfalls.suntimealerts.ui.theme
+
+import androidx.compose.ui.unit.dp
+
+object AppSpacing {
+    val xxs = 4.dp
+    val xs = 8.dp
+    val s = 12.dp
+    val m = 16.dp
+    val l = 20.dp
+    val xl = 24.dp
+    val xxl = 32.dp
+}
+
+val AppCornerRadius = 12.dp
