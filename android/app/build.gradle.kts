2cf4768278903dba24d91173156a3bcbdd0ea130
bfalls <bfalls@outlook.com>
2026-01-01 16:05:40 -0700

    Align Android SDK/API levels for Play Store

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,14 +6,12 @@ plugins {
 
 android {
     namespace = "com.bfalls.suntimealerts"
-    compileSdk {
-        version = release(36)
-    }
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.bfalls.suntimealerts"
-        minSdk = 24
-        targetSdk = 36
+        minSdk = 26
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
