3b82f46cac63ac50af2f4e03e6fb85bcce90a9a6
bfalls <bfalls@outlook.com>
2026-09-04 15:11:59 -0600

    bump version

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
         applicationId = "com.bfalls.suntimealerts"
         minSdk = 26
         targetSdk = 36
-        versionCode = 2
+        versionCode = 3
         versionName = "0.1.0-alpha01"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
